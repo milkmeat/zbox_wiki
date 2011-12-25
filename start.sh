48a75893337762005960fa22e7b6984f559bc5e1
Shuge Lee <shuge.lee@gmail.com>
2011-12-25 21:47:11 +0800

    Small changed, update default port from '10000' to '9001' in start.sh script.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-spawn-fcgi -d `pwd`  -f `pwd`/main.py -a 127.0.0.1 -p 10000 -u www-data -g www-data -P `pwd`/pid
+spawn-fcgi -d `pwd`  -f `pwd`/main.py -a 127.0.0.1 -p 9001 -u www-data -g www-data -P `pwd`/pid
 
